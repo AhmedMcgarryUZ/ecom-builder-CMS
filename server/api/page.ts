b49218b9f42ac0da8ec5b8451a0f861a8d595bba
Ahmed McGarry <130065744+AhmedMcgarryUZ@users.noreply.github.com>
2024-06-15 01:19:05 +0100

    Working on plugging in pages

--- server/api/page.ts
+++ server/api/page.ts
@@ -3,6 +3,7 @@ export default defineEventHandler(() => {
     id: '',
     attributes: {
       slug: '/',
+      title: 'Homepage',
       published: true,
       widgets: [
         {
@@ -10,13 +11,50 @@ export default defineEventHandler(() => {
           content: 'Free standard delivery when you spend £45 or more!',
           id: 1,
           parentId: 'root',
-          slots: []
+          slots: [],
+          children: []
         },
         {
           is: 'BannerLarge',
           id: 2,
           parentId: 'root',
-          slots: []
+          slots: [],
+          children: []
+        },
+        {
+          is: 'ContainerCarousel',
+          title: 'Shop Latest Drops',
+          subtitle: 'Mens',
+          id: 3,
+          parentId: 'root',
+          slots: [4],
+          children: [],
+          options: {
+            perPage: 3
+          }
+        },
+        {
+          is: 'CardSimple',
+          id: 4,
+          parentId: 3,
+          slots: [],
+          children: []
+        },
+        {
+          is: 'ContainerCarousel',
+          title: 'Shop by Category',
+          subtitle: 'Mens',
+          id: 5,
+          parentId: 'root',
+          slots: [6],
+          children: []
+        },
+        {
+          is: 'CardSimple',
+          id: 6,
+          parentId: 3,
+          slots: [],
+          children: []
         }
       ]
     }
---
+++ types/widget.ts
@@ -0,0 +1,7 @@
+export interface Widget {
+    id: string,
+    is: string,
+    parentId: string,
+    children: string[],
+    slots: string[]
+}
